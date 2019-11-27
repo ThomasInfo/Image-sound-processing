a64d65252966902e0e3a42235c9de7354a90c6ad
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-11-27 15:06:25 +0100

    libpng and cimg added

---
+++ cmake-build-debug/CMakeFiles/Image_sound_processing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Image_sound_processing.dir/main.cpp.o"
+  "Image_sound_processing"
+  "Image_sound_processing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Image_sound_processing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "libpng-1.6.37/png.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}

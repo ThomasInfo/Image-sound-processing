4c7aaba0dd8e2cfd581205cddab94f48cf67cd9c
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-11-29 14:05:26 +0100

    remove .idea

---
+++ src/Reader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sofia on 29/11/2019.
+//
+
+#include "Reader.h"
---
+++ src/Reader.h
@@ -0,0 +1,14 @@
+//
+// Created by Sofia on 29/11/2019.
+//
+
+#ifndef IMAGE_SOUND_PROCESSING_READER_H
+#define IMAGE_SOUND_PROCESSING_READER_H
+
+
+class Reader {
+
+};
+
+
+#endif //IMAGE_SOUND_PROCESSING_READER_H

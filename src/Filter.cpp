a2482a4a352deaf8d62aaa362efe94b59fe1497e
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-12-06 11:58:52 +0100

    cmake list merge

---
+++ src/Filter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Sofia on 04/12/2019.
+//
+
+#include "Filter.h"
---
+++ src/Filter.h
@@ -0,0 +1,14 @@
+//
+// Created by Sofia on 04/12/2019.
+//
+
+#ifndef IMAGE_PROCESSING_FILTER_H
+#define IMAGE_PROCESSING_FILTER_H
+
+
+class Filter {
+
+};
+
+
+#endif //IMAGE_PROCESSING_FILTER_H

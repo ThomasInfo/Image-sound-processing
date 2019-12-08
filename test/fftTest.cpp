930778846a685b300b6d8c265dd5389dc6f48c0a
Thomas <thomas.fry@epfl.ch>
2019-12-08 11:00:37 +0100

    FFT Tests

--- test/fftTest.cpp
+++ test/fftTest.cpp
@@ -4,60 +4,49 @@
 
 #include "gtest/gtest.h"
 #include "../src/Reader.h"
+#include "../src/FFT.cpp"
 
 
-TEST (ReaderTest, RGBImageIsLoaded) {
-
-    Reader r;
-
-    RGBImage image = r.loadRGBImage("../images/mandrill.png");
-
-    //Correct height
-    EXPECT_EQ(image.size(),480);
-    //Correct width
-    EXPECT_EQ(image[0].size(),480);
-    //Correct spectrum
-    EXPECT_EQ(image[0][0].size(),3);
-}
-
-TEST (ReaderTest, GreyScaleImageIsLoaded) {
+TEST (fftTest, GreyScaleFFTIsComputed) {
 
     Reader r;
-
     Channel image = r.loadGSImage("../images/lenna.jpeg");
 
-    //Correct height
+    vector<vector<complex<double>>> GS_FFT = Fast_Fourier_Transform(image);
+
+    //Correct size
     EXPECT_EQ(image.size(),512);
     //Correct width
     EXPECT_EQ(image[0].size(),512);
-
 }
 
-TEST (ReaderTest, RGBChannelsAreLoaded) {
+
+TEST (fftTest, ColorsFFTAreComputed) {
 
     Reader r;
-
     Channel red = r.extractRedChannel("../images/mandrill.png");
     Channel green = r.extractGreenChannel("../images/mandrill.png");
     Channel blue = r.extractBlueChannel("../images/mandrill.png");
 
-    //Correct height
-    EXPECT_EQ(red.size(),480);
-    //Correct width
-    EXPECT_EQ(red[0].size(),480);
-
+    vector<vector<complex<double>>> red_FFT = Fast_Fourier_Transform(red);
+    vector<vector<complex<double>>> green_FFT = Fast_Fourier_Transform(green);
+    vector<vector<complex<double>>> blue_FFT = Fast_Fourier_Transform(blue);
 
     //Correct height
-    EXPECT_EQ(green.size(),480);
+    EXPECT_EQ(red_FFT.size(),480);
     //Correct width
-    EXPECT_EQ(green[0].size(),480);
+    EXPECT_EQ(red_FFT[0].size(),480);
+
 
     //Correct height
-    EXPECT_EQ(blue.size(),480);
+    EXPECT_EQ(green_FFT.size(),480);
     //Correct width
-    EXPECT_EQ(blue[0].size(),480);
+    EXPECT_EQ(green_FFT[0].size(),480);
 
-
+    //Correct height
+    EXPECT_EQ(blue_FFT.size(),480);
+    //Correct width
+    EXPECT_EQ(blue_FFT[0].size(),480);
 }
 
 
